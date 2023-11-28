daf129143b1abd6041a6ff56304e868a2f5fca59
Robbie Hudekker <hudekker@gmail.com>
2023-11-29 00:08:15 +0800

    1

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 const cards = document.querySelectorAll('.memory-card');
+let numberOfGuesses = 0;
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+// JavaScript Functions
+function openModal() {
+  var modal = document.getElementById("myModal");
+  document.querySelector('#guess-counter').innerText = numberOfGuesses;
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+  document.getElementById('play-again-btn').classList.remove('no-display');
+}
+
+function updateGuessesDisplay() {
+  document.getElementById('guesses-display').innerText = `猜測次數: ${numberOfGuesses}`;
+}
+
+function updateGameStatus() {
+  const remainingBackCards = document.querySelectorAll('.memory-card:not(.flip)');
+
+  if (remainingBackCards.length === 0) {
+    openModal();
+
+
+    // Game completed, show modal or perform other actions
+    console.log('Game completed!');
+    // You can trigger your modal or any other actions here
+  }
+}
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
+
 
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    numberOfGuesses++;
+    updateGuessesDisplay();
     return;
   }
 
@@ -23,7 +57,13 @@ function flipCard() {
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  isMatch ? disableCards() : unflipCards();
+  if (isMatch) {
+    disableCards();
+    updateGameStatus();
+
+  } else {
+    unflipCards();
+  }
 }
 
 function disableCards() {
@@ -49,6 +89,22 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+// JavaScript Function
+function playAgain() {
+  // Add logic to reset the game state, shuffle cards, etc.
+  closeModal();
+  resetGame(); // You need to implement this function to reset the game state
+  window.location.reload(); // Reload the page
+}
+
+// Call the resetBoard function wherever you want to reset the game state
+function resetGame() {
+  resetBoard(); // Assuming you have a resetBoard function to reset the game state
+  guessCounter = 0; // Reset the guess counter
+  // updateGuessCounter(); // Update the guess counter display
+}
+
+
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
@@ -56,6 +112,13 @@ function resetBoard() {
   });
 })();
 
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target.id == 'myModal') {
+    closeModal()
+  }
+}
 // Simplified event listeners
 cards.forEach(card => {
   card.addEventListener('click', flipCard);
